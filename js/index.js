c2c74a32cbb34268fea9310b8b58528722b5a3d0
youngi-yaw <zixing.r@gmail.com>
2015-11-12 13:57:31 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,16 @@
 $(function() {
     console.log( "ready!" );
+            window.onresize = autoResizeDiv;
+        autoResizeDiv();
 
     myScroll2 = new IScroll('#scroll2', {
     scrollX: false, scrollY: true, mouseWheel: true, scrollbars: true, interactiveScrollbars: true, shrinkScrollbars: 'scale',
     fadeScrollbars: true
     });
 });
+
+        function autoResizeDiv()
+        {
+            document.getElementById('main').style.height = window.innerHeight +'px';
+        }
+
