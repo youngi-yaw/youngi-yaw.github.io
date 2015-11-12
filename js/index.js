088233355edfa8e8abb68ff8f5ef3a6831d37f85
youngi-yaw <zixing.r@gmail.com>
2015-11-12 13:55:24 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 $(function() {
     console.log( "ready!" );
-    $.getScript('iscroll.js');
 
     myScroll2 = new IScroll('#scroll2', {
     scrollX: false, scrollY: true, mouseWheel: true, scrollbars: true, interactiveScrollbars: true, shrinkScrollbars: 'scale',
